bde579dc0fbb1c721d93c495d4d9dcb096fb9f8e
Nur Rabby Shuvo <106588083+0xnurrabby@users.noreply.github.com>
2026-03-25 06:39:59 +0600

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="base:app_id" content="69c3144cf58f3fd1ddcf3f31" />
       <body>{children}</body>
     </html>
   );
