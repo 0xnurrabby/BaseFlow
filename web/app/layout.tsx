10d06ecff93db6ab2e2eaa09853274ce0c560d1d
Nur Rabby Shuvo <106588083+0xnurrabby@users.noreply.github.com>
2026-03-25 06:41:31 +0600

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,16 +4,9 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "BaseFlow",
   description: "Funky boxy Base multisender",
-  other: {
-    "base:app:id": "69c3144cf58f3fd1ddcf3f31",
-  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <meta name="base:app_id" content="69c3144cf58f3fd1ddcf3f31" />
